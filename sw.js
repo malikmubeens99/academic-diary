849e6f72049e2ce3f0d2fff489af51bd4e88eb7c
malik mubeen <mubeensarfraz99@gmail.com>
2026-09-13 11:24:35 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='academic-diary-v2';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.json'])))});
-self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('./index.html')))});
