02a96fe717ad62b3cc74a5c03af62fa5fb94230f
malik mubeen <mubeensarfraz99@gmail.com>
2026-09-13 10:32:05 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "academic-diary-3a12d.firebaseapp.com",
+  projectId: "academic-diary-3a12d",
+  storageBucket: "academic-diary-3a12d.firebasestorage.app",
+  messagingSenderId: "654645926256",
+  appId: "1:654645926256:web:872c7cb1ad3425af4e17b5",
+  measurementId: "G-SZE0CLZ4L0"
+};
